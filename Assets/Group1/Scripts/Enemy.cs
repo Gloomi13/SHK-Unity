d3041d43bbad09d8c94605794f2710b88550c851
Gloomi13 <Gloomi13@mail.ru>
2022-08-31 11:54:59 +1000

    Refactoring

---
+++ Assets/Group1/Scripts/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private Vector3 Target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Tar();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, Target, 2 * Time.deltaTime);
+        if (transform.position == Target)
+            Tar();
+    }
+
+        void Tar()
+    {
+        Target = Random.insideUnitCircle * 4;
+    }
+}
